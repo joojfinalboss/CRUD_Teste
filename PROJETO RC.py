aa2ef557d10775f9848a4841a189e6b04e3e59de
joojfinalboss <joaovitor02.2000@gmail.com>
2023-08-21 22:34:28 -0300

    v2.0 - MODULARIZAÇÃO

--- PROJETO RC.py
+++ PROJETO RC.py
@@ -1,15 +1,9 @@
 # NOME: João Vitor Ferreira Antunes de Lima
 # CURSO: Tecnologia em Análise e Desenvolvimento de Sistemas
 
-# IMPORTANTE
 
-
-menu_atual = "1" # O valor "menu_atual" é utilizado para navegação entre os menus.
-lista_estudantes = []
-
-# MENU PRINCIPAL
-# Loop inicial para apresentação do menu principal.
-while menu_atual == "1":
+# FUNÇOES
+def menu():
     print("-----MENU PRINCIPAL-----")
     print("1. Gerenciar Estudantes")
     print("2. Gerenciar Disciplinas")
@@ -17,8 +11,101 @@ while menu_atual == "1":
     print("4. Gerenciar Turmas")
     print("5. Gerenciar Matrículas")
     print("0. Sair")
-    proximo = input("Informe o menu desejado: ")        # O sistema recebe o valor de "proximo", que irá determinar qual será o próximo loop a ser rodado.
-    if proximo == "0" or proximo.lower() == "sair":             # Caso a opção de sair seja selecionada, "menu_atual" recebe o valor de 0 para impedir que o loop do menu principal seja executado novamente, e o programa encerra.
+
+def submenu():
+    print("1. Incluir")
+    print("2. Listar")
+    print("3. Atualizar")
+    print("4. Excluir")
+    print("0. Menu Principal")
+    funcao = input("Informe a operação desejada: ")
+    if funcao == "1" or funcao.lower() == "incluir":
+        incluir()
+    elif funcao == "2" or funcao.lower() == "listar":
+        listar()
+    elif funcao == "3" or funcao.lower() == "atualizar":
+        atualizar()
+    elif funcao == "4" or funcao.lower() == "excluir":
+        excluir()
+    elif funcao == "0" or funcao.lower() == "menu principal":
+        menu_atual = "1".
+    else:
+        print()
+        print("Operação Selecionada: INVÁLIDA. Tente novamente\n")
+
+def incluir(lista):
+        print()
+        print("Operação Selecionada: === INCLUIR ===")
+        try:
+            cod = int(input("Digite o código: "))
+            nome = str(input("Digite o nome: "))
+            cpf = int(input("Digite o CPF: "))
+            cadastro = [cod, nome, cpf]
+            lista.append(cadastro)
+            print("Usuário cadastrado com sucesso.\n")
+        except ValueError:
+            print("Ação inválida. Tente novamente.")
+
+def atualizar(lista):
+    print()
+    print("Operação Selecionada: === ATUALIZAR ===")
+    if len(lista) == 0:
+        print("Não há usuários cadastrados\n")
+    else:
+        try:
+            cod = int(input("Informe o código do usuário a ser editado: "))
+            for registro in lista:
+                if cod == registro[0]:
+                    registro[0] = int(input("Digite o código: "))
+                    registro[1] = str(input("Digite o nome: "))
+                    registro[2] = int(input("Digite o CPF: "))
+                else:
+                    print("Código inválido. Tente novamente.")
+        except ValueError:
+            print("Ação inválida. Tente novamente.")
+
+def listar(lista):
+    print()
+    print("Operação Selecionada: === LISTAR ===")
+    if len(lista) == 0:
+        print("Não há usuários cadastrados\n")
+    else:
+        for i in range(len(lista)):
+            cadastro = lista[i]
+            print("Código: ", cadastro[0])
+            print("Nome: ", cadastro[1])
+            print("CPF: ", cadastro[2], "\n")
+
+def excluir(lista):
+    print()
+    print("Operação Selecionada: === EXCLUIR ===")
+    if len(lista) == 0:  # Caso não haja itens na lista, executará este loop.
+        print("Não há usuários cadastrados\n")
+    else:
+        try:
+            cod = int(input("Informe o código do usuário a ser excluído: "))
+            for registro in lista_estudantes:
+                if cod == registro[0]:
+                    lista.remove(registro)
+                    print("O usuário foi excluído com sucesso.\n")
+                else:
+                    print("Código inválido. Tente novamente.")
+        except ValueError:
+            print("Ação inválida. Tente novamente.")
+
+# IMPORTANTE
+
+
+menu_atual = "1" # O valor "menu_atual" é utilizado para navegação entre os menus.
+lista_estudantes = []
+lista_professores = []
+
+# MENU PRINCIPAL
+# Loop inicial para apresentação do menu principal.
+while menu_atual == "1":
+    menu()
+    proximo = input("Informe o menu desejado: ")
+    if proximo == "0" or proximo.lower() == "sair":
         print("Encerrando aplicação. Até a próxima! =)")
         menu_atual = "0"
 
@@ -26,87 +113,10 @@ while menu_atual == "1":
 
     elif proximo == "1" or proximo.lower() == "gerenciar estudantes":
         menu_atual = "2"
+        lista = lista_estudantes
         while menu_atual == "2":
             print("---{ESTUDANTES} Menu de Operações---")
-            print()
-            print("1. Incluir")
-            print("2. Listar")
-            print("3. Atualizar")
-            print("4. Excluir")
-            print("0. Menu Principal")
-            funcao = input("Informe a operação desejada: ")  # "funcao" recebe um valor que determina qual será o próximo loop a ser executado.
-            if funcao == "1" or funcao.lower() == "incluir":
-                print()
-                print("Operação Selecionada: === INCLUIR ===")
-                try:
-                    cod = int(input("Digite o código do estudante: "))
-                    nome = str(input("Digite o nome do estudante: "))
-                    cpf = int(input("Digite o CPF do estudante: "))
-                    cadastro = [cod, nome, cpf]
-                    lista_estudantes.append(cadastro)
-                    print("Estudante cadastrado com sucesso.\n")  # Após isso, o loop do menu de operações é executado novamente.
-                except ValueError:
-                    print("Ação inválida. Tente novamente.")
-
-            elif funcao == "2" or funcao.lower() == "listar":
-                print()
-                print("Operação Selecionada: === LISTAR ===")
-                if len(lista_estudantes) == 0:  # Caso não haja itens na lista, executará este loop.
-                    print("Não há estudantes cadastrados\n")
-                else:
-                    for i in range(
-                            len(lista_estudantes)):  # Este loop exibirá cada item armazenado em "lista_estudantes", e será executado de acordo com quantos itens existem nesta lista.
-                        cadastro = lista_estudantes[i]
-                        print("Código: ", cadastro[0])
-                        print("Nome: ", cadastro[1])
-                        print("CPF: ", cadastro[2], "\n")
-
-            # Caso selecionem atualizar ou excluir, retornará uma mensagem.
-
-            elif funcao == "3" or funcao.lower() == "atualizar":
-                print()
-                print("Operação Selecionada: === ATUALIZAR ===")
-                if len(lista_estudantes) == 0:  # Caso não haja itens na lista, executará este loop.
-                    print("Não há estudantes cadastrados\n")
-                else:
-                    try:
-                        cod = int(input("Informe o código do aluno a ser editado: "))
-                        for registro in lista_estudantes:
-                            if cod == registro[0]:
-                                registro[0] = int(input("Digite o código do estudante: "))
-                                registro[1] = str(input("Digite o nome do estudante: "))
-                                registro[2] = int(input("Digite o CPF do estudante: "))
-                            else:
-                                print("Código inválido. Tente novamente.")
-                    except ValueError:
-                        print("Ação inválida. Tente novamente.")
-            elif funcao == "4" or funcao.lower() == "excluir":
-                print()
-                print("Operação Selecionada: === EXCLUIR ===")
-                if len(lista_estudantes) == 0:  # Caso não haja itens na lista, executará este loop.
-                    print("Não há estudantes cadastrados\n")
-                else:
-                    try:
-                        cod = int(input("Informe o código do aluno a ser excluído: "))
-                        for registro in lista_estudantes:
-                            if cod == registro[0]:
-                                lista_estudantes.remove(registro)
-                                print("O cadastro do estudante foi excluído com sucesso.\n")
-                            else:
-                                print("Código inválido. Tente novamente.")
-                    except ValueError:
-                        print("Ação inválida. Tente novamente.")
-
-            # Caso selecionem o menu principal, "menu_atual" receberá o valor 1, que forçará a execução do loop do menu principal.
-            elif funcao == "0" or funcao.lower() == "menu principal":
-                menu_atual = "1"
-            # Caso o que o usuário digite não seja válido, exibirá uma mensagem.
-            else:
-                print()
-                print("Operação Selecionada: INVÁLIDA. Tente novamente\n")
-
-
-    # Os próximos loops apresentam uma mensagem caso o usuário selecione outras opções além de GERENCIAR ESTUDANTES no MENU PRINCIPAL.
+            submenu()
 
     # DISCIPLINAS
 
@@ -116,8 +126,11 @@ while menu_atual == "1":
     # PROFESSORES
 
     elif proximo == "3" or proximo.lower() == "gerenciar professores":
-        print("EM DESENVOLVIMENTO\n")
-
+        menu_atual = "4"
+        lista = lista_professores
+        while menu_atual = "4"
+            print("---{ESTUDANTES} Menu de Operações---")
+            submenu()
     # TURMAS
 
     elif proximo == "4" or proximo.lower() == "gerenciar turmas":
